9672d666195aace715320e6f1982835f260a2692
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2025-03-26 18:30:54 -0300

    Erasing test code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.write('Veja o histórico das cotações das empresas.')
 """
 ticker = st.sidebar.text_input(
     'Escolha o ticker:',
-    value = ['TSLA', 'AAPL']
+    value = 'TSLA'
     )
 """
 
@@ -24,29 +24,6 @@ tickers = st.sidebar.multiselect(
 #fig = plot_price(ticker)
 #st.plotly_chart(fig)
 
-if tickers:
-    for ticker in tickers:
-        fig = plot_price(ticker)
-        st.plotly_chart(fig)
-else:
-    st.write("Selecione pelo menos um ticker para visualizar o gráfico.")
-
-
-
-
-
-
-# Lista de opções disponíveis
-tickers_disponiveis = ['TSLA', 'AAPL', 'MSFT', 'GOOGL', 'AMZN']
-
-# Permite que o usuário selecione múltiplos tickers
-tickers = st.sidebar.multiselect(
-    'Escolha os tickers:',
-    options=tickers_disponiveis,
-    default=['TSLA']
-)
-
-# Gera e exibe os gráficos para cada ticker selecionado
 if tickers:
     for ticker in tickers:
         fig = plot_price(ticker)
